2d7a517994e42c31557c21166e21b7dfdff2693e
Jean-Milost Reymond <jean_milost@hotmail.com>
2025-02-06 22:20:21 +0100

    - cleaned: code and comments

--- Demos/Platformer/Platformer.cpp
+++ Demos/Platformer/Platformer.cpp
@@ -26,7 +26,7 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                   *
  ****************************************************************************/
 
- // std
+// std
 #include <memory>
 
 // classes
